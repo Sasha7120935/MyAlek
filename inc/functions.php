94943e6da0181f11886c5b9a46aba0b94438c647
Sasha7120935 <belousalek@gmail.com>
2019-07-07 19:04:56 +0300

    changed function

--- inc/functions.php
+++ inc/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function helloW()
+function showMePiNumber()
 {
-    echo 'Hello world';
+    echo pi();
 }
--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 require_once 'inc/functions.php';
 
-helloW();
+showMePiNumber();
